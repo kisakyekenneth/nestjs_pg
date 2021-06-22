ccc060bbfa8cc5b75622c39dbeb3973638e9c641
kiken-eng <kanyecope@gmail.com>
2021-06-22 18:23:16 +0300

    Defining our Database Schema that is the attributes

---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { TaskStatus } from './tasks.model';
+
+//@Entity decorator tells nest that the class is a DB entity
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: TaskStatus;
+}
