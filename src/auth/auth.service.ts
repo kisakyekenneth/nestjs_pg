d19409375a78743c66fcac207a634e2f0180cd33
kiken-eng <kanyecope@gmail.com>
2021-07-18 08:28:42 +0300

    Added bcrypt, signin and signup logics in both files

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,5 +13,22 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
+  }
+
+  //User login
+  //User login
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+
+    //Finding if the provided userName exists in the database
+    const user = await this.userRepository.findOne({ username });
+
+    //If username exists then compare given 'password' with user.password in the DB this is done with bcrypt
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'success';
+    } else {
+      //if the user and password don't exist then use in-built method to throw error
+      throw new UnauthorizedException('Please check your credentials');
+    }
   }
 }
