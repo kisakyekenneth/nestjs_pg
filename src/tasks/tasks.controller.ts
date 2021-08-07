8ad126668eac76c2aafb2c5e01f4b2826cc6f033
kiken-eng <kanyecope@gmail.com>
2021-08-07 20:22:11 +0300

    Added UseGuards

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,14 +7,18 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
-
+// To protect an entire route, import AuthModule under tasks module
+//Then use the @useGuard() which protects users from navigating without token
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
